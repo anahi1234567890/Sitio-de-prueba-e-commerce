8f8ca092aeb722dd90831814ea33e66348ed213a
Anahi Buendia <60759729+anahi1234567890@users.noreply.github.com>
2022-09-28 21:55:34 -0500

    Update e-commerce.py

--- e-commerce.py
+++ e-commerce.py
@@ -1,21 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium .webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path=r"D:\User\Anahi\Documents\Escuela\SEMESTRE_7\SQA\chromedriver.exe")
-action = ActionChains(driver)
 url = "https://demoblaze.com/index.html"
 
 driver.maximize_window()
 driver.get(url)
-time.sleep(3)
+time.sleep(2)
 
 #Registro
 driver.find_element(By.XPATH,"//*[@id='signin2']").click()
-time.sleep(3)
+time.sleep(2)
+
+usuario = driver.find_element(By.XPATH,"//*[@id='sign-username']")
+usuario.send_keys("Anahi")
+password = driver.find_element(By.XPATH,"//*[@id='sign-password']")
+password.send_keys("aft5890A")
+usuario.send_keys(Keys.ENTER)
 
 #Ingresar
+driver.find_element(By.XPATH,"//*[@id='signInModal']/div/div/div[3]/button[2]").click()
+time.sleep(2)
+
 
 #Agregar productos
 
