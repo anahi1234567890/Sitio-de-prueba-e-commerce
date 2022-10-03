ca9d0c13630858f4ee24ffe3458f6b3c9bb9cc75
Anahi Buendia <60759729+anahi1234567890@users.noreply.github.com>
2022-10-03 15:17:10 -0500

    Update e-commerce.py

--- e-commerce.py
+++ e-commerce.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium .webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
 import time
 
 driver = webdriver.Chrome(executable_path=r"D:\User\Anahi\Documents\Escuela\SEMESTRE_7\SQA\chromedriver.exe")
@@ -8,27 +9,48 @@ url = "https://demoblaze.com/index.html"
 
 driver.maximize_window()
 driver.get(url)
+alert = Alert(driver)
+
+
+#Sign up
+def sign_up():
+    driver.find_element(By.ID, "signin2").click()
+    time.sleep(2)
+
+    user = driver.find_element(By.ID,"sign-username")
+    user.send_keys("Anahi")
+    password = driver.find_element(By.ID,"sign-password")
+    password.send_keys("aft5890A")
+    user.send_keys(Keys.ENTER)
+
+    driver.find_element(By.XPATH, "//*[@id='signInModal']/div/div/div[3]/button[2]").click()
+    time.sleep(2)
+    alert.accept()
+    
+#Login
+def login():
+    driver.find_element(By.ID, "login2").click()
+    time.sleep(2)
+
+    user = driver.find_element(By.ID,"loginusername")
+    user.send_keys("Anahi")
+    password = driver.find_element(By.ID,"loginpassword")
+    password.send_keys("aft5890A")
+    user.send_keys(Keys.ENTER)
+
+    driver.find_element(By.XPATH, "//*[@id='logInModal']/div/div/div[3]/button[2]").click()
+    time.sleep(2)
+
+#Add to cart
+def add_cart():
+
+    articles = {
+            "Phones" : ['//*[@id="tbodyid"]/div[4]/div/div/h4/a', '//*[@id="tbodyid"]/div[7]/div/div/h4/a'],
+            "Laptops" : ['//*[@id="tbodyid"]/div[5]/div/div/h4/a', '//*[@id="tbodyid"]/div[2]/div/div/h4/a'],
+            "Monitors" : ['//*[@id="tbodyid"]/div[1]/div/div/h4/a', '//*[@id="tbodyid"]/div[2]/div/div/h4/a']
+            }
+
+
+login()
+add_cart()
 time.sleep(2)
-
-#Registro
-driver.find_element(By.XPATH,"//*[@id='signin2']").click()
-time.sleep(2)
-
-usuario = driver.find_element(By.XPATH,"//*[@id='sign-username']")
-usuario.send_keys("Anahi")
-password = driver.find_element(By.XPATH,"//*[@id='sign-password']")
-password.send_keys("aft5890A")
-usuario.send_keys(Keys.ENTER)
-
-#Ingresar
-driver.find_element(By.XPATH,"//*[@id='signInModal']/div/div/div[3]/button[2]").click()
-time.sleep(2)
-
-
-#Agregar productos
-
-#Carrito de compras
-
-#Comprar (Rellenar formulario)
-
-#Generar reporte
